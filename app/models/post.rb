254b159f0a12177d0b1fea15ffc8e9865cabadd5
Melissa Real <melissa.real@hotmail.com>
2019-01-06 17:31:44 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,16 @@ class Post < ActiveRecord::Base
  validates :content, length: { minimum: 250 }
  validates :summary, length: { maximum: 250 }
  validates :category, inclusion:  { in: %w(Fiction Non-Fiction) }
+
+
+
+   def validate_clickbait(title)
+     options = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+       options.each do |check|
+         unless title.include?(check)
+           errors.add(:title, "The title needs to be clickbait")
+         end
+       end
+   end
+
 end
