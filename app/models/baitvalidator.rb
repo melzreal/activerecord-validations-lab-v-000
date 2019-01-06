36456519254f11c37f52215a49fcef738b380fa6
Melissa Real <melissa.real@hotmail.com>
2019-01-06 17:26:04 +0000

    Automatically backed up by Learn

--- app/models/baitvalidator.rb
+++ app/models/baitvalidator.rb
@@ -1,10 +1,10 @@
-class BaitValidator < ActiveModel::Validator
+class BaitValidator < ActiveModel::EachValidator
   def validate(title)
     options = ["Won't Believe", "Secret", "Top [number]", "Guess"]
       options.each do |check|
         unless title.include?(check)
           record.errors[:title] << 'The title needs to be clickbait'
         end
-      end 
+      end
   end
 end
